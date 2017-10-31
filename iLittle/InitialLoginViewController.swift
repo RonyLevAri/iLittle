0cfaeba954fc4a5dd587f3ab04e3a8fab1dc57de
Rony Lev-Ari <ronylevari@gmail.com>
2017-10-31 22:36:23 +0200

    Added initial notification authorization unfinished flow functions

--- iLittle/InitialLoginViewController.swift
+++ iLittle/InitialLoginViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import UserNotifications
 
 class InitialLoginViewController: UIViewController {
+    
+    //MARK: actions
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "kusemek"
+        content.body = "arsabuk"
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        let notificationRequest = UNNotificationRequest(identifier: "identifier", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(notificationRequest, withCompletionHandler: {(error) in
+            if let error = error {
+                print(error)
+            } else {
+                print("notification scheduled")
+            }
+        })
+    }
+    
+    func doInitialAuthorizationFlow() {
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings(completionHandler: { (settings) in
+            if(settings.authorizationStatus == .authorized) {
+                // go to configuration screen
+                self.scheduleNotification()
+            } else {
+                // User has not given permissions
+                center.requestAuthorization(options: [.sound, .badge, .alert], completionHandler: {(granted, error) in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        if(granted) {
+                            // go to configuration screen
+                            self.scheduleNotification()
+                        }
+                    }
+                })
+            }
+        })
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
 
