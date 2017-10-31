7249de15c5664da7f434353109f6a5e2de5404cb
Rony Lev-Ari <ronylevari@gmail.com>
2017-10-31 22:19:33 +0200

    Configured firebase to work with application

--- iLittle/AppDelegate.swift
+++ iLittle/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        Auth.auth().signIn(withEmail: "tahat@gmail.com", password: "123456") { (user, error) in
+            print(user?.email ?? "hara lo logged in")
+        }
         return true
     }
 
