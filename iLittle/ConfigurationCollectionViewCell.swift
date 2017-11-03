c9f707a124f37cdc4d73e711e22c216f8544af86
Rony Lev-Ari <ronylevari@gmail.com>
2017-11-03 23:24:57 +0200

    Added custom collectionViewCell (and xib file) for configuration screen's notification categories

---
+++ iLittle/ConfigurationCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewCell.swift
+//  iLittle
+//
+//  Created by rony_temp on 03/11/2017.
+//  Copyright © 2017 rony_temp. All rights reserved.
+//
+
+import UIKit
+
+class ConfigurationCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: properties
+    @IBOutlet weak var checkFeedback: UIImageView!
+    @IBOutlet weak var icon: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
