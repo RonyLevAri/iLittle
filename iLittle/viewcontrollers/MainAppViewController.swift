74b9eece64ffbc6bbd0ed7f922a46ce0b1f26a70
Rony Lev-Ari <ronylevari@gmail.com>
2017-11-04 17:04:04 +0200

    added main app view controller

---
+++ iLittle/viewcontrollers/MainAppViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainAppControlScreenViewController.swift
+//  iLittle
+//
+//  Created by rony_temp on 04/11/2017.
+//  Copyright © 2017 rony_temp. All rights reserved.
+//
+
+import UIKit
+
+class MainAppViewController: UIViewController {
+
+    //MARK: propertires
+    var username: String?
+    var data = [NotificationCategory]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
